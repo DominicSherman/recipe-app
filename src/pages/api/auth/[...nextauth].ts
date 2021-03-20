6741f666841fb89d8c4050b697e41145b3b70aa3
Dominic Sherman <dominic.sherman98@gmail.com>
2021-03-20 09:22:35 -0700

    Add prisma (#1)

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import Adapters from 'next-auth/adapters';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -36,4 +40,7 @@ export default NextAuth({
   jwt: {
     secret: process.env.JWT_SECRET,
   },
+  adapter: Adapters.Prisma.Adapter({
+    prisma,
+  }),
 });
