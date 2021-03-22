c1ce51634c125c01f124c617297974d55ed2a59a
Dominic Sherman <dominic.sherman98@gmail.com>
2021-03-22 15:41:01 -0700

    Fix things

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
 import 'css/global.css';
-import NextApp from 'next/app';
 
 import { GlobalStyles, Meta } from 'components';
 import { Provider as NextAuthProvider } from 'next-auth/client';
 import { ToastProvider } from 'react-toast-notifications';
-import { passwordProtect } from 'services/app-service';
 
 function App({ Component, pageProps }) {
   return (
@@ -17,13 +15,5 @@ function App({ Component, pageProps }) {
     </NextAuthProvider>
   );
 }
-
-App.getInitialProps = async (appContext) => {
-  const appProps = await NextApp.getInitialProps(appContext);
-
-  passwordProtect(appContext.ctx);
-
-  return { ...appProps };
-};
 
 export default App;
