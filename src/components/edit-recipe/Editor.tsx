8d35c7a4da039c74f9aa9246fa5ff805fbf172d3
Dominic Sherman <dominic.sherman98@gmail.com>
2021-04-08 10:21:09 -0700

    Add static toolbar (#17)

--- src/components/edit-recipe/Editor.tsx
+++ src/components/edit-recipe/Editor.tsx
@@ -1,10 +1,20 @@
 import DraftEditor from '@draft-js-plugins/editor';
 import React from 'react';
+import '@draft-js-plugins/static-toolbar/lib/plugin.css';
 
 import { useEditorContext } from './editor-context';
 import { useSetEditingRecipeTitle } from './hooks';
 import { useHandleKeyCommand, useKeybindingFunction } from './keybindings';
-import { plugins } from './plugins';
+import { plugins, Toolbar, Separator } from './plugins';
+import {
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  BoldButton,
+  ItalicButton,
+  UnorderedListButton,
+  OrderedListButton,
+  HeadlineThreeButton,
+} from '@draft-js-plugins/buttons';
 
 export const Editor = () => {
   const { editorState, onChange, editorRef } = useEditorContext();
@@ -14,16 +24,38 @@ export const Editor = () => {
   useSetEditingRecipeTitle();
 
   return (
-    <DraftEditor
-      editorKey="SimpleInlineToolbarEditor"
-      editorState={editorState}
-      handleKeyCommand={handleKeyCommand}
-      keyBindingFn={keybindingFunction}
-      onChange={onChange}
-      plugins={plugins}
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      ref={editorRef}
-    />
+    <div className="editor">
+      <DraftEditor
+        editorKey="SimpleInlineToolbarEditor"
+        editorState={editorState}
+        handleKeyCommand={handleKeyCommand}
+        keyBindingFn={keybindingFunction}
+        onChange={onChange}
+        plugins={plugins}
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        ref={editorRef}
+      />
+      <Toolbar>
+        {(externalProps) => (
+          <>
+            <HeadlineOneButton {...externalProps} />
+            <HeadlineTwoButton {...externalProps} />
+            <HeadlineThreeButton {...externalProps} />
+            <Separator />
+            <BoldButton {...externalProps} />
+            <ItalicButton {...externalProps} />
+            <Separator />
+            <UnorderedListButton {...externalProps} />
+            <OrderedListButton {...externalProps} />
+          </>
+        )}
+      </Toolbar>
+      <style jsx>{`
+        .editor :global(.public-DraftEditor-content) {
+          min-height: 500px;
+        }
+      `}</style>
+    </div>
   );
 };
