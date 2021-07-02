a72a1f48834b7c84d228afd6407f01c7d6a8a51e
Dominic Sherman <dominic.sherman98@gmail.com>
2021-07-02 13:56:12 -0500

    Test vercel env

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     ALGOLIA_APPLICATION_ID: process.env.ALGOLIA_APPLICATION_ID,
     ALGOLIA_SEARCH_API_KEY: process.env.ALGOLIA_SEARCH_API_KEY,
+    VERCEL_ENV: process.env.VERCEL_ENV,
   },
   images: {
     domains: [`${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`],
--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,10 @@ export enum Env {
   PROD = 'production',
 }
 
-export const getEnv = (): Env => (process.env.VERCEL_ENV as Env) || Env.DEV;
+export const getEnv = (): Env => {
+  console.log({ foo: process.env.VERCEL_ENV });
+
+  return (process.env.VERCEL_ENV as Env) || Env.DEV;
+};
 
 export const isProduction = (): boolean => getEnv() === Env.PROD;
