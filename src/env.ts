97abd82f619f95640633ee01c8d16e125b676821
Dominic Sherman <dominic.sherman98@gmail.com>
2021-07-02 14:00:46 -0500

    Yeet

--- src/env.ts
+++ src/env.ts
@@ -5,8 +5,6 @@ export enum Env {
 }
 
 export const getEnv = (): Env => {
-  console.log({ env: process.env.NEXT_PUBLIC_VERCEL_ENV });
-
   return (process.env.NEXT_PUBLIC_VERCEL_ENV as Env) || Env.DEV;
 };
 
