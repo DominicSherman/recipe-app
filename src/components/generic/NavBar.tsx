1ca40ce6ce53ffe5f5ae2cf76a03a6b0aedb9969
Dominic Sherman <dominic.sherman98@gmail.com>
2021-11-05 11:27:41 -0700

    Make navbar mobile responsive (#29)

--- src/components/generic/NavBar.tsx
+++ src/components/generic/NavBar.tsx
@@ -1,49 +1,71 @@
+import { Disclosure } from '@headlessui/react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
 import * as Options from 'enums/options';
-import { useSession, signIn, signOut } from 'next-auth/client';
+import { signIn, signOut, useSession } from 'next-auth/client';
 import Link from 'next/link';
 import React from 'react';
+
+const navigation = [
+  { name: 'Home', href: '/' },
+  { name: 'Search Recipes', href: '/search-recipes' },
+  { name: 'Search Users', href: '/search-users' },
+  { name: 'My Recipes', href: '/my-recipes' },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
 
 export const NavBar = () => {
   const [session] = useSession();
 
   return (
-    <div>
-      <nav className="bg-secondary">
-        <div className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
-              <div className="flex-shrink-0">
-                <Link href="/">
-                  <a className="text-primary text-3xl font-extrabold font-satisfy">
-                    {Options.APP_TITLE}
-                  </a>
-                </Link>
+    <Disclosure as="nav" className="bg-secondary">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-primary hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon aria-hidden="true" className="block h-6 w-6" />
+                  ) : (
+                    <MenuIcon aria-hidden="true" className="block h-6 w-6" />
+                  )}
+                </Disclosure.Button>
               </div>
-            </div>
-            <div>
-              <div className="ml-4 flex items-center md:ml-6">
-                <Link href="/">
-                  <a>
-                    <p className="mr-4">Home</p>
-                  </a>
-                </Link>
-                <Link href="/search-recipes">
-                  <a>
-                    <p className="mr-4">Search Recipes</p>
-                  </a>
-                </Link>
-                <Link href="/search-users">
-                  <a>
-                    <p className="mr-4">Search Users</p>
-                  </a>
-                </Link>
-                {session ? (
-                  <Link href="/my-recipes">
-                    <a>
-                      <p className="mr-4">My Recipes</p>
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 items-center hidden sm:flex">
+                  <Link href="/">
+                    <a className="text-primary text-3xl font-extrabold font-satisfy">
+                      {Options.APP_TITLE}
                     </a>
                   </Link>
-                ) : null}
+                </div>
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        // aria-current={item.current ? 'page' : undefined}
+                        className={classNames(
+                          //item.current
+                          false
+                            ? 'bg-gray-900 text-white'
+                            : 'text-primary hover:bg-gray-300',
+                          'px-3 py-2 rounded-md text-sm font-medium'
+                        )}
+                        href={item.href}
+                        key={item.name}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button className="btn" onClick={session ? signOut : signIn}>
                   {session ? 'Logout' : 'Login'}
                 </button>
@@ -71,8 +93,30 @@ export const NavBar = () => {
               </div>
             </div>
           </div>
-        </div>
-      </nav>
-    </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  // aria-current={item.current ? 'page' : undefined}
+                  as="a"
+                  className={classNames(
+                    // item.current
+                    false
+                      ? 'bg-gray-900 text-white'
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  href={item.href}
+                  key={item.name}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 };
